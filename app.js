27a9d9082a7ad00f8e7d9ce3571bb6f9bf7033bc
sircoca <35822010+sircoca@users.noreply.github.com>
2022-11-11 14:55:49 -0300

    correcao

--- app.js
+++ app.js
@@ -234,9 +234,7 @@ app.post('/send-media', async (req, res) => {
 
   const media = new MessageMedia(mimetype, attachment, fileName);
 
-  client.sendMessage(number, media, {
-    caption: caption
-  }).then(response => {
+  client.sendMessage(number, media).then(response => {
     res.status(200).json({
       status: true,
       response: response
